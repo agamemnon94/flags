a5f4bc01d48d0f27c37995f2e14f5b124335f51a
Agamemnon94 <94798272+agamemnon94@users.noreply.github.com>
2023-02-09 00:22:53 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>À propos</h1>
+      <br />
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, hic blanditiis fugit culpa tempora ipsam quis facilis dolorum harum labore placeat repellendus similique, reiciendis voluptas nulla quidem unde amet, quisquam distinctio voluptates quam fugiat ullam adipisci. Repudiandae expedita, iure eligendi id corporis dicta minus unde autem exercitationem dolores, modi dolore beatae, eos ut perspiciatis quis! Reiciendis dolore eius consectetur quidem, dolor enim quod autem quos laboriosam magni corporis temporibus optio dicta ipsa sint nemo quo aliquam dolorem architecto sunt dignissimos. Enim, iusto dolorem? Labore cumque facilis commodi qui sed, pariatur amet quidem officiis enim saepe odio optio modi voluptate maxime?</p>
+      <br />
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quidem quas eum inventore culpa esse reprehenderit, eaque maxime cupiditate rerum qui ab repudiandae autem. Tempore perspiciatis quod ab deserunt voluptatibus sed similique vitae quae iste veniam, soluta, tempora cum necessitatibus doloremque consequuntur quis consequatur? Placeat quas vitae perferendis hic vel dolorem voluptas incidunt repudiandae quam distinctio temporibus cum, tempore delectus mollitia eius! Illo at voluptatem ducimus laborum ab consectetur quod distinctio soluta, voluptatibus magni adipisci quisquam quaerat ad laboriosam iure doloremque maiores. Neque vitae cumque officiis. Sunt fugiat delectus tenetur ad perferendis doloremque itaque vitae omnis autem facilis, architecto harum magnam mollitia natus qui ipsam aperiam ipsa nam cupiditate ex voluptatibus hic quisquam ut in. Ratione pariatur perspiciatis, non quas alias nulla cum fugit autem dicta. Provident doloribus fugiat in maiores consequatur, consequuntur quisquam incidunt quidem ut harum ratione similique aliquid ullam ex adipisci perspiciatis! Temporibus molestias, dolorem esse ex expedita at deserunt cupiditate, beatae explicabo nihil quis facilis repellat repudiandae fuga magnam. Odit cupiditate omnis sit maxime fugiat impedit modi neque quasi tenetur mollitia, aliquam cum nisi adipisci aperiam esse ipsum veritatis dolorum at a natus soluta quos ea inventore voluptas! Placeat nulla esse ipsa corporis vitae enim veritatis.</p>
+    </div>
+  );
+};
+
+export default About;
