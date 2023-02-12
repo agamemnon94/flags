88474f34edb13bb48f1368c8d43792ef7f1dbc25
Agamemnon94 <94798272+agamemnon94@users.noreply.github.com>
2023-02-12 05:54:11 +0100

    update blog

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,20 +1,41 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Logo from '../Components/Logo';
 import Navigation from '../Components/Navigation';
+import axios from 'axios';
+import Article from '../Components/Article';
 
 
 const Blog = () => {
+
+  const [blogData, setBlogData] = useState([]);
+  const [author, setAuthor] = useState("");
   const [content, setContent] = useState("");
   const [error, setError] = useState(false);
+
+  // On récupère les données de la BD locale db.json
+  const getData = () => {
+    axios
+      .get("http://localhost:3004/articles")
+      .then((res) => setBlogData(res.data));
+  };
+
+  useEffect(() => getData(), []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (content.length < 140) {
-      // alert("Noonn");
       setError(true);
     } else {
+      axios.post("http://localhost:3004/articles", {
+        author,
+        content,
+        date: Date.now(),
+      });
       setError(false);
+      setAuthor("");
+      setContent("");
+      getData();
     }
   }
 
@@ -25,18 +46,31 @@ const Blog = () => {
       <Navigation />
       <h1>Blog</h1>
       <form onSubmit={(e) => handleSubmit(e)} >
-        <input type="text" placeholder='Nom' />
+        <input
+          type="text"
+          placeholder='Nom'
+          onChange={(e) => setAuthor(e.target.value)}
+          value={author}
+        />
         <textarea
           // ↓ Style conditionnel ↓
           style={{ border: error ? "1px solid red" : "1px solid #61dafb" }}
-          placeholder='Message' onChangeCapture={(e) => setContent(e.target.value, setError(false))}></textarea>
+          placeholder='Message'
+          onChange={(e) => setContent(e.target.value)}
+          value={content}
+        ></textarea>
         {/* ↓ Affichage conditionnel du p : si error est true alors on affiche le <p></p> ↓ */}
         {error && <p >Votre message doit contenir un minimum de 140 caractères</p>}
         <input type="submit" value="Envoyer" />
       </form>
 
       <ul>
-
+        {/* On pacours la bd "article"*/}
+        {blogData
+          .sort((a, b) => b.date - a.date)
+          .map((article) => (
+            <Article key={article.id} article={article} />
+          ))}
       </ul>
     </div>
   );
